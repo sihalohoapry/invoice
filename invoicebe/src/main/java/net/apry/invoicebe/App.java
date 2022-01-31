04ca1cdf6fc2354e8c7ea9e1e6663ff5773534e6
Apry Madona Sihaloho <madonaapry@gmail.com>
2022-01-31 13:45:30 +0700

    setup project

---
+++ invoicebe/src/main/java/net/apry/invoicebe/App.java
@@ -0,0 +1,13 @@
+package net.apry.invoicebe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
