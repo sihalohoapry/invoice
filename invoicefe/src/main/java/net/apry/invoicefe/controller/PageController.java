ca28b5ef920479495da5d5ee021ec84d8e236b5a
Apry Madona Sihaloho <madonaapry@gmail.com>
2022-01-31 16:29:49 +0700

    setup project

---
+++ invoicefe/src/main/java/net/apry/invoicefe/controller/PageController.java
@@ -0,0 +1,17 @@
+package net.apry.invoicefe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= { "/","/home","/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Be Greet");
+		return mv;
+	}
+
+}
